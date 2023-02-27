9847801f7b488fe01831c472bc65fa49cc29e8bf
Viktor Mazur <market.brocars@gmail.com>
2023-02-27 07:58:34 -0800

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by YAQRUT on 2023-02-27.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
